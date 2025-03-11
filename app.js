4ce96a306a01f10266fc656744e466ed1a746eb8
LDLNAN <lucas-donald@live.nmit.ac.nz>
2025-03-12 10:26:39 +1300

    Initial Commit, learning var/let/const basics.

---
+++ app.js
@@ -0,0 +1,36 @@
+//const firstName = 'Lucas'
+// const lastName = 'Donald'
+// const wantDeath = true
+// Camel case when naming variables
+//Invalid --
+// first-name
+// 1_num
+// num_#_1
+// 
+const classCode = 'SDV503'
+
+// Declaring different variables of different data types
+
+let firstName
+let lastName
+let student
+let country
+let age
+
+[firstName, lastName, student, country, age] = ["Lucas", "Donald", true, "New Zealand", 23]
+
+let [firstName2, lastName2, student2, country2, age2] = 
+[
+    "Lucas", 
+    "Donald", 
+    true, 
+    "New Zealand",
+    23
+]
+
+let myAge = 23, yourAge = "Old"
+
+console.log("I am "+myAge+" years old.") 
+console.log("You are "+yourAge+".")
+
+
